ef3ede8ed871a0e2f8e1956ca73fad7e396208e9
Olivia Kesler <oakesler@protonmail.ch>
2019-07-05 21:08:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,10 @@
-require "pry"
+
 def speak_to_grandma(phrase)
   if phrase.scan(/[a-z]/)
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.scan(/[A-Z]/)
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else
-    "NO, NOT SINCE 1938!"
-    
-    binding.pry
   end
 end
