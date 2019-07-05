27aca23f836812c6fb5b301889bdb37582d97fa4
Olivia Kesler <oakesler@protonmail.ch>
2019-07-05 21:17:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(phrase)
   upper = phrase.upcase
   if phrase != upper
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == upper
-    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == upper
+    return "NO, NOT SINCE 1938!"
   end
 end
